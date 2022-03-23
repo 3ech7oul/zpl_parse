8a71874cc2a0f461fb4ebc2e9242470f1fc7239e
Denis Nichiporenko <deni@boozt.com>
2022-03-23 15:52:35 +0100

    Pdf package refactoring

--- pdf/pdfPrinter.go
+++ pdf/pdfPrinter.go
@@ -19,16 +19,18 @@ var ZplPdfHandlers = map[string]HandlerFunc{
 type HandlerFunc func(c zpl.Command, p gopdf.GoPdf) gopdf.GoPdf
 
 type Pdf struct {
-	Pdf gopdf.GoPdf
+	pdf         gopdf.GoPdf
+	zplCommands []zpl.Command
 }
 
-func CreatePdf() Pdf {
+func CreatePdf(zpl []zpl.Command) Pdf {
 	p := gopdf.GoPdf{}
 	p.Start(gopdf.Config{PageSize: *gopdf.PageSizeA4})
 	p.AddPage()
 
 	return Pdf{
-		Pdf: p,
+		pdf:         p,
+		zplCommands: zpl,
 	}
 }
 
